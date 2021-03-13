52eae1777284ef1499b2297b167468a8afee5fda
OusmanaTraore <traoreosman@yahoo.fr>
2021-03-13 19:55:55 +0100

    second commit, adding brightness

--- display_brightness/src/app/app.module.ts
+++ display_brightness/src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+// Import plugin
+import { Brightness } from '@ionic-native/brightness/ngx';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    Brightness,
+    { 
+      provide: RouteReuseStrategy, 
+      useClass: IonicRouteStrategy 
+    }
+  ],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {}
--- display_brightness/src/app/home/home.page.ts
+++ display_brightness/src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Brightness } from '@ionic-native/brightness/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+ brightnessValue = 0.42;
+
+ constructor(private brightnessModule: Brightness) {
+   this.brightnessModule.setBrightness(this.brightnessValue);
+ }
+
+ controlBrightness(){
+   this.brightnessModule.setBrightness(this.brightnessValue);
+ }  
 
 }
